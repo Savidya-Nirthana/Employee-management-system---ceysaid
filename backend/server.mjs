1e7ae4054467706bd24fb6abfdeef1af13eef364
Savidya Perera <snirthana5@gmail.com>
2025-04-03 02:45:17 +0530

    V01-C23

--- backend/server.mjs
+++ backend/server.mjs
@@ -11,9 +11,10 @@ import { logger } from "./middlewares/logger.js";
 const server = express();
 
 const corsOptions = {
-  origin: '*',  // Allow all origins
+  origin: 'http://localhost:5173',  // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 };
 
 
