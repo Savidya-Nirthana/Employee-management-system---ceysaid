c6e700bf982630fde3e0473a75f7fc9aa85b6f24
Savidya Perera <snirthana5@gmail.com>
2025-04-03 02:24:13 +0530

    C01-V18

--- backend/server.mjs
+++ backend/server.mjs
@@ -30,7 +30,7 @@ server.use("/api/v1/sales", salesRouter);
 server.get("/api/v1", (req, res) => {
   res
     .status(200)
-    .json({ message: "testing", environment: process.env.NODE_ENV });
+    .json({ message: "testingkkk", environment: process.env.NODE_ENV });
 });
 
 server.use(notFound);
